344f93b31813eb45e0937a76611b6ccab1e3b25e
cazbyu <131417207+cazbyu@users.noreply.github.com>
2025-12-01 10:41:26 -0700

    Updated ConditionalProviders.tsx

--- contexts/ConditionalProviders.tsx
+++ contexts/ConditionalProviders.tsx
@@ -1,5 +1,4 @@
-import React, { useMemo } from 'react';
-import { usePathname } from 'expo-router';
+import React from 'react';
 import { AuthenticScoreProvider } from './AuthenticScoreContext';
 import { TabResetProvider } from './TabResetContext';
 
@@ -7,36 +6,12 @@ interface ConditionalProvidersProps {
   children: React.ReactNode;
 }
 
-const AUTHENTICATED_ROUTES = [
-  '/(tabs)',
-  '/calendar',
-  '/followup',
-  '/reflections',
-  '/settings',
-  '/coach',
-  '/suggestions',
-];
-
 export function ConditionalProviders({ children }: ConditionalProvidersProps) {
-  const pathname = usePathname();
-
-  const needsAuthProvider = useMemo(() => {
-    return AUTHENTICATED_ROUTES.some(route => pathname?.startsWith(route));
-  }, [pathname]);
-
-  if (needsAuthProvider) {
-    return (
-      <TabResetProvider>
-        <AuthenticScoreProvider>
-          {children}
-        </AuthenticScoreProvider>
-      </TabResetProvider>
-    );
-  }
-
   return (
     <TabResetProvider>
-      {children}
+      <AuthenticScoreProvider>
+        {children}
+      </AuthenticScoreProvider>
     </TabResetProvider>
   );
 }
