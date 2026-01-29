267d6128f4a38ea50c5aecd67d20d555db058c9e
cazbyu <131417207+cazbyu@users.noreply.github.com>
2026-01-29 06:48:07 -0700

    Updated index.ts

---
+++ components/northStar/index.ts
@@ -0,0 +1,6 @@
+// North Star Landing Page Components
+// Export all tab components for the North Star page
+
+export { MyVisionTab } from './MyVisionTab';
+export { CoachsCornerTab } from './CoachsCornerTab';
+export { SparkLibraryTab } from './SparkLibraryTab';
