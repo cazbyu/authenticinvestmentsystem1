df175f7db0896220798baf7b1168a6f0b2b1ae3a
cazbyu <131417207+cazbyu@users.noreply.github.com>
2026-01-29 09:57:27 -0700

    Updated _layout.web.tsx

--- app/_layout.web.tsx
+++ app/_layout.web.tsx
@@ -5,8 +5,8 @@ import { ThemeProvider } from '@/contexts/ThemeContext';
 import { AuthenticScoreProvider } from '@/contexts/AuthenticScoreContext';
 import { TabResetProvider } from '@/contexts/TabResetContext';
 import { MorningSparkProvider } from '@/contexts/MorningSparkContext';
-import { SideMenu } from '@/components/SideMenu';
 import { HeaderColorProvider } from '@/contexts/HeaderColorContext';
+import { SideMenu } from '@/components/SideMenu';
 import React from 'react';
 
 console.log('[App] _layout.web.tsx loaded');
@@ -19,35 +19,37 @@ export default function RootLayout() {
     <ThemeProvider>
       <AuthenticScoreProvider>
         <TabResetProvider>
-          <MorningSparkProvider>
-            <Drawer
-              drawerContent={() => <SideMenu />}
-              screenOptions={{
-                headerShown: false,
-                drawerStyle: {
-                  width: 280,
-                },
-              }}
-            >
-              <Drawer.Screen name="index" />
-              <Drawer.Screen name="landing" />
-              <Drawer.Screen name="login" />
-              <Drawer.Screen name="(tabs)" />
-              <Drawer.Screen name="calendar" />
-              <Drawer.Screen name="followup" />
-              <Drawer.Screen name="reflections" />
-              <Drawer.Screen name="settings" />
-              <Drawer.Screen name="coach" />
-              <Drawer.Screen name="terms" />
-              <Drawer.Screen name="privacy" />
-              <Drawer.Screen name="about" />
-              <Drawer.Screen name="contact" />
-              <Drawer.Screen name="suggestions" />
-              <Drawer.Screen name="auth/callback" />
-              <Drawer.Screen name="+not-found" />
-            </Drawer>
-            <StatusBar style="auto" />
-          </MorningSparkProvider>
+          <HeaderColorProvider>
+            <MorningSparkProvider>
+              <Drawer
+                drawerContent={() => <SideMenu />}
+                screenOptions={{
+                  headerShown: false,
+                  drawerStyle: {
+                    width: 280,
+                  },
+                }}
+              >
+                <Drawer.Screen name="index" />
+                <Drawer.Screen name="landing" />
+                <Drawer.Screen name="login" />
+                <Drawer.Screen name="(tabs)" />
+                <Drawer.Screen name="calendar" />
+                <Drawer.Screen name="followup" />
+                <Drawer.Screen name="reflections" />
+                <Drawer.Screen name="settings" />
+                <Drawer.Screen name="coach" />
+                <Drawer.Screen name="terms" />
+                <Drawer.Screen name="privacy" />
+                <Drawer.Screen name="about" />
+                <Drawer.Screen name="contact" />
+                <Drawer.Screen name="suggestions" />
+                <Drawer.Screen name="auth/callback" />
+                <Drawer.Screen name="+not-found" />
+              </Drawer>
+              <StatusBar style="auto" />
+            </MorningSparkProvider>
+          </HeaderColorProvider>
         </TabResetProvider>
       </AuthenticScoreProvider>
     </ThemeProvider>
